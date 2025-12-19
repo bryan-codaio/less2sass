118355df84e3b7b6404dd585b28611bba41313b9
Bryan Jensen <bryan.jensen@grammarly.com>
2025-12-18 18:24:46 -0800

    Let prettier auto-format

--- index.js
+++ index.js
@@ -1,133 +1,129 @@
 // http://stackoverflow.com/questions/14970224/anyone-know-of-a-good-way-to-convert-from-less-to-sass
 
-function Less2Sass(){
+function Less2Sass() {}
 
-}
-
-Less2Sass.prototype.convert = function(file) {
-
+Less2Sass.prototype.convert = function (file) {
   this.file = file;
 
   this.convertInterpolatedVariables()
-      .convertReferenceImports()
-      .convertUrlImports()
-      .convertVariables()
-      .convertTildaStrings()
-      .includeMixins()
-      .convertMixins()
-      .convertExtend()
-      .convertColourHelpers()
-      .convertFileExtensions()
-      .wrapWithInterpolation()
-      .convertFunctionUnit();
+    .convertReferenceImports()
+    .convertUrlImports()
+    .convertVariables()
+    .convertTildaStrings()
+    .includeMixins()
+    .convertMixins()
+    .convertExtend()
+    .convertColourHelpers()
+    .convertFileExtensions()
+    .wrapWithInterpolation()
+    .convertFunctionUnit();
 
   return this.file;
 };
 
-Less2Sass.prototype.convertReferenceImports = function() {
+Less2Sass.prototype.convertReferenceImports = function () {
   var includeRegex = /^@import\s*\(reference\)/gm;
 
-  this.file = this.file.replace(includeRegex, '@import');
+  this.file = this.file.replace(includeRegex, "@import");
 
   return this;
 };
 
-Less2Sass.prototype.convertUrlImports = function() {
+Less2Sass.prototype.convertUrlImports = function () {
   var includeRegex = /^@import url\((.*)\);/gm;
 
-  this.file = this.file.replace(includeRegex, '@import $1;');
+  this.file = this.file.replace(includeRegex, "@import $1;");
 
   return this;
 };
 
-Less2Sass.prototype.includeMixins = function() {
+Less2Sass.prototype.includeMixins = function () {
   var includeRegex = /^(\s*)\.([a-zA-Z][\w\-]*\(?[^;{}]*\)?;( \/\/ .+)?$)/gm;
 
-  this.file = this.file.replace(includeRegex, '$1@include $2');
+  this.file = this.file.replace(includeRegex, "$1@include $2");
 
   return this;
 };
 
-
-Less2Sass.prototype.convertMixins = function() {
+Less2Sass.prototype.convertMixins = function () {
   // Simple form: no semicolons.
   const mixinRegexNoSemicolon = /^(\s*?)\.([\w\-]*?)\s*\(([\s\S][^\;]+?)?\)\s*\{$/gm;
-  this.file = this.file.replace(mixinRegexNoSemicolon, '$1@mixin $2($3) {');
+  this.file = this.file.replace(mixinRegexNoSemicolon, "$1@mixin $2($3) {");
   // With semicolons.
   const mixinRegexWithSemicolon = /^(\s*?)\.([\w\-]*?)\s*\(([\s\S][^\,]+?)?\)\s*\{$/gm;
   this.file = this.file.replace(mixinRegexWithSemicolon, function (match, g1, g2, g3) {
-    return g1 + '@mixin ' + g2 + '(' + g3.replace(/;/g, ',') + ') {';
+    return g1 + "@mixin " + g2 + "(" + g3.replace(/;/g, ",") + ") {";
   });
   return this;
 };
 
-Less2Sass.prototype.convertFunctionUnit = function() {
+Less2Sass.prototype.convertFunctionUnit = function () {
   // Two-args.
   const unitTwoArgRegex = /unit\((\S+),(\S+)\)/g;
-  this.file = this.file.replace(unitTwoArgRegex, '0$2 + $1');
+  this.file = this.file.replace(unitTwoArgRegex, "0$2 + $1");
   // One-arg.
   const unitOneArgRegex = /unit\(([^,]+)\)/g;
-  this.file = this.file.replace(unitOneArgRegex, 'unit-less($1)');
+  this.file = this.file.replace(unitOneArgRegex, "unit-less($1)");
 
   return this;
 };
 
-Less2Sass.prototype.convertExtend = function() {
+Less2Sass.prototype.convertExtend = function () {
   // http://lesscss.org/features/#extend-feature
   // &:extend syntax.
   const andExtendRegex = /&:extend\((.[\w]*)\);/g;
-  this.file = this.file.replace(andExtendRegex, '@extend $1;');
+  this.file = this.file.replace(andExtendRegex, "@extend $1;");
 
   return this;
 };
 
-Less2Sass.prototype.convertColourHelpers = function() {
+Less2Sass.prototype.convertColourHelpers = function () {
   var helperRegex = /spin\(/g;
 
-  this.file = this.file.replace(helperRegex, 'adjust-hue(');
+  this.file = this.file.replace(helperRegex, "adjust-hue(");
 
   // TODO (EK): Flag other colour helpers for manual conversion that SASS does not have
 
   return this;
 };
 
-Less2Sass.prototype.convertTildaStrings = function() {
+Less2Sass.prototype.convertTildaStrings = function () {
   var tildaRegex = /~("|')/g;
 
-  this.file = this.file.replace(tildaRegex, '$1');
+  this.file = this.file.replace(tildaRegex, "$1");
 
   return this;
 };
 
-Less2Sass.prototype.convertInterpolatedVariables = function() {
+Less2Sass.prototype.convertInterpolatedVariables = function () {
   var interpolationRegex = /@\{(?!(\s|\())/g;
 
-  this.file = this.file.replace(interpolationRegex, '#{$');
+  this.file = this.file.replace(interpolationRegex, "#{$");
 
   return this;
 };
 
-Less2Sass.prototype.convertVariables = function() {
+Less2Sass.prototype.convertVariables = function () {
   // Matches any @ that doesn't have 'media ' or 'import ' after it.
   var atRegex = /@(?!(media|import|mixin|font-face|keyframes|kr-modules|supports|value)(\s|\(|\/))/g;
 
-  this.file = this.file.replace(atRegex, '$');
+  this.file = this.file.replace(atRegex, "$");
 
   return this;
 };
 
-Less2Sass.prototype.convertFileExtensions = function() {
+Less2Sass.prototype.convertFileExtensions = function () {
   var extensionRegex = /\.less\b/g;
 
-  this.file = this.file.replace(extensionRegex, '.scss');
+  this.file = this.file.replace(extensionRegex, ".scss");
 
   return this;
 };
 
-Less2Sass.prototype.wrapWithInterpolation = function() {
+Less2Sass.prototype.wrapWithInterpolation = function () {
   var mediaAndVariableRegex = /^(\s*@media\s+)(\$.*)(\s+\{)/gm;
 
-  this.file = this.file.replace(mediaAndVariableRegex, '$1#{$2}$3');
+  this.file = this.file.replace(mediaAndVariableRegex, "$1#{$2}$3");
 
   return this;
 };
